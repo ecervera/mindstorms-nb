7349246f19cf3b7998df903e3598de7ea937c55a
Enric Cervera <ecervera@uji.es>
2016-12-23 18:29:13 +0100

    nxt motor test

--- nxt/functions.py
+++ nxt/functions.py
@@ -15,17 +15,22 @@ def connect(n):
     except BluetoothError as e:
         errno, errmsg = eval(e.args[0])
         if errno==16:
-            print("No es pot connectar, ja hi ha un altre programa. Has de desconnectar-lo abans!")
+            print("\x1b[31mNo es pot connectar, ja hi ha un altre programa. Has de desconnectar-lo abans!\x1b[0m")
         elif errno==13:
-            print("No es pot connectar, el dispositiu no està emparellat.")
+            print("\x1b[31mNo es pot connectar, el dispositiu no està emparellat.\x1b[0m")
         elif errno == 112:
-            print("No es troba el brick, assegurat que estiga encés.")
+            print("\x1b[31mNo es troba el brick, assegurat que estiga encés.\x1b[0m")
         else:
             print("Error %d: %s" % (errno, errmsg))
+    except KeyError:
+        print("\x1b[31mNúmero de robot incorrecte.\x1b[0m")
 
-def disconnect(brick):
-    brick.sock.close()
-
+def disconnect():
+    try:
+        brick.sock.close()
+    except NameError:
+        print("\x1b[31mNo hi ha connexió amb el robot.\x1b[0m")
+'''
 def forward():
     mB.run(-100)
     mC.run(100)
@@ -33,7 +38,36 @@ def forward():
 def backward():
     mB.run(100)
     mC.run(-100)
-    
+'''
 def stop():
     mB.brake()
     mC.brake()
+
+def forward(speed=100,speed_B=100,speed_C=100):
+    move(speed_B=min(abs(speed),abs(speed_B)),speed_C=min(abs(speed),abs(speed_C)))
+    
+def backward(speed=100,speed_B=100,speed_C=100):
+    move(speed_B=-min(abs(speed),abs(speed_B)),speed_C=-min(abs(speed),abs(speed_C)))
+    
+def left(speed=100):
+    move(speed_B=0,speed_C=abs(speed))
+
+def right(speed=100):
+    move(speed_B=abs(speed),speed_C=0)
+
+def move(speed_B=0,speed_C=0):
+    max_speed = 100
+    speed_B = int(speed_B)
+    speed_C = int(speed_C)
+    if speed_B > 100:
+        speed_B = 100
+        print("\x1b[33mLa velocitat màxima és 100.\x1b[0m")
+    if speed_C > 100:
+        speed_C = 100
+        print("\x1b[33mLa velocitat màxima és 100.\x1b[0m")
+    try:
+        mB.run(-int(speed_B*max_speed/100))
+        mC.run(int(speed_C*max_speed/100))
+    except NameError:
+        print("\x1b[31mNo hi ha connexió amb el robot.\x1b[0m")
+  
